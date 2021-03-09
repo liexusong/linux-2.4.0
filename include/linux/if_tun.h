ae03bb12a8190e6f9f001e64b2201a0ee89f821f
liexusong1 <xusong.lie@cheezz.cc>
2021-03-09 11:40:54 +0800

    add comments

--- include/linux/if_tun.h
+++ include/linux/if_tun.h
@@ -33,11 +33,11 @@
 
 struct tun_struct {
     char                    name[8];      // TUN的名字
-    unsigned long           flags;        // 类型: TUN或者TAP
+    unsigned long           flags;        // 设备类型: TUN或者TAP
     struct fasync_struct    *fasync;
     wait_queue_head_t       read_wait;    // 等待队列
     struct net_device       dev;          // 设备结构
-    struct sk_buff_head     txq;          // 传输队列(接收到的数据会保存到这里)
+    struct sk_buff_head     txq;          // 数据队列(接收到的数据会保存到这里)
     struct net_device_stats stats;        // 设备统计数据
 
 #ifdef TUN_DEBUG
