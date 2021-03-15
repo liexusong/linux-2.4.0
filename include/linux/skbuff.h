f9d437f08d5e89a6645157144c67f2122c8ec0ab
liexusong1 <xusong.lie@cheezz.cc>
2021-03-15 11:25:45 +0800

    add comments

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -885,8 +885,9 @@ static inline void skb_queue_purge(struct sk_buff_head *list)
 {
 	struct sk_buff *skb;
 
-	while ((skb = skb_dequeue(list)) != NULL)
+	while ((skb = skb_dequeue(list)) != NULL) {
 		kfree_skb(skb);
+	}
 }
 
 /**
